899f7c2c30db65dcf4d4e0cf6969cfcfed71ad92
RebeckaK <kallenrebecka@gmail.com>
2021-04-25 17:13:36 +0200

    new

---
+++ Krusty.py
@@ -0,0 +1,161 @@
+from bottle import get, post, run, request, response
+import sqlite3
+from urllib.parse import quote, unquote
+
+
+db = sqlite3.connect("database.sqlite")
+
+@post('/reset')
+def reset():
+    c = db.cursor()
+    c.executescript(
+        """
+        DELETE FROM Cookie_ingredients;
+        DELETE FROM Material_storage;
+        DELETE FROM Cookies;
+        DELETE FROM Order_details;
+        DELETE FROM Customers;
+        DELETE FROM Pallets;
+        DELETE FROM Orders;
+        DELETE FROM Deliveries;
+        """
+    )
+    response.status = 205
+    db.commit()
+    return {"location": "/"}
+
+
+@post('/customers')
+def post_customers():
+    customer = request.json
+    c = db.cursor()
+    try:
+        c.execute(
+            """
+            INSERT
+            INTO Customers(customer_name, address)
+            VALUES (?, ?)
+            """,
+            [customer['name'], customer['address']]
+        )
+        response.status = 201
+        db.commit()
+        return {"location": "/customers/" + quote(customer['name'])}
+    except:
+        response.status = 400
+        return {""}
+
+
+@get('/customers')
+def get_customers():
+    c = db.cursor()
+    c.execute(
+        """
+        SELECT  customer_name, address
+        FROM    Customers
+        """
+    )
+    found = [{"name": customer_name, "address": address} for customer_name, address in c]
+    response.status = 200
+    return {"data": found}
+
+
+@post('/ingredients')
+def post_ingredients():
+    ingredient = request.json
+    c = db.cursor()
+    try:
+        c.execute(
+            """
+            INSERT
+            INTO Material_storage(ingredient, unit, current_amount)
+            VALUES (?, ?, 0)
+            """,
+            [ingredient['ingredient'], ingredient['unit']]
+        )
+        response.status = 201
+        db.commit()
+        return {"location": "/ingredients/" + quote(ingredient['ingredient'])}
+    except:
+        response.status = 400
+        return {""}
+
+
+@post('/ingredients/<ingredients>/deliveries')
+def post_ingredients(ingredients):
+    ingredient = request.json
+    c = db.cursor()
+    try:
+        c.execute(
+            """
+            UPADTE Material_storage
+            SET delivery_time = ?,
+                current_amount = current_amount + ?,
+                last_deposit = ?
+            WHERE ingredient LIKE ?
+            """,
+            [ingredient['deliveryTime'], ingredient['quantity'].replace("_", ""), ingredient['quantity'].replace("_", ""), ingredients]
+        )
+        response.status = 201
+        db.commit()
+        found = [{"ingredient": ingredient, "quantity": current_amount, "unit": unit} for ingredient, current_amount, unit in c]
+        return {"data": found}
+    except:
+        response.status = 400
+        return {""}
+
+
+@get('/ingredients')
+def get_ingredients():
+    ingredient = request.json
+    c = db.cursor()
+    try:
+        c.execute(
+            """
+            SELECT ingredient, current_amount, unit
+            FROM Material_storage
+            """
+        )
+        response.status = 201
+        db.commit()
+        found = [{"ingredient": ingredient, "quantity": current_amount, "unit": unit} for ingredient, current_amount, unit in c]
+        return {"data": found}
+    except:
+        response.status = 400
+        return {""}
+
+
+@post('/cookies')
+def post_cookies():
+    cookie = request.json
+    recipe = [cookie["name"] + {ingredient, amount} for cookie["ingredient"], cookie["amount"] in cookie["recipe"]]
+    c = db.cursor()
+    #found = [{"ingredient": ingredient, "quantity": current_amount, "unit": unit} for ingredient, current_amount, unit in c]
+    try:
+        c.execute(
+            """
+            INSERT
+            INTO Cookies(cookie_name)
+            VALUES ?
+            """,
+            [cookie['name']]
+        )
+        db.commit()
+        c.executemany(
+            """
+            INSERT
+            INTO Cookie_ingredients(cookie_name, ingredient, amount)
+            VALUES (?,?,?)
+            """,
+            recipe
+            #[cookie["name"], for cookie["ingredient"], cookie["amount"] in cookie["recipe"]]
+        );
+        response.status = 201
+        db.commit()
+        return {"location": "/cookies/" + cookie['name']}
+    except:
+        response.status = 400
+        return {""}
+
+
+run(host='localhost', port=8888)
--- database.sql
+++ database.sql
@@ -11,14 +11,14 @@ DROP TABLE IF EXISTS Material_storage;
 CREATE TABLE Material_storage(
 	ingredient      TEXT PRIMARY KEY,
     current_amount  INT,
-    date            DATE,
-    time            TIME,
-    last_deposit    INT
+    delivery_time   DATETIME,
+    last_deposit    INT,
+    unit            TEXT
 );
 
-DROP TABLE IF EXISTS Cookie;
+DROP TABLE IF EXISTS Cookies;
 
-CREATE TABLE Cookie(
+CREATE TABLE Cookies(
 	cookie_name     TEXT PRIMARY KEY
 );
 
@@ -27,8 +27,7 @@ DROP TABLE IF EXISTS Pallets;
 CREATE TABLE Pallets(
 	pallet_id CHAR(4) PRIMARY KEY,
 	cookie_name TEXT,
-	date DATE,
-	time TIME,
+    pallet_time DATETIME,
 	delivery_status INT,  -- 1 - Delivered, 0 - Not delivered
 	blocked INT,        -- 1 - Blocked, 0 - Not blocked
 	order_id CHAR(4)
@@ -54,8 +53,7 @@ DROP TABLE IF EXISTS Deliveries;
 CREATE TABLE Deliveries(
 	delivery_id		CHAR(4),
 	order_id		CHAR(4),
-	date			DATE,
-	time			TIME
+	delivery_time	DATETIME
 );
 
 DROP TABLE IF EXISTS Orders;
